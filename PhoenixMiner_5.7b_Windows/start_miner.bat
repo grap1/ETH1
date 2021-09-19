ba7945c2f395f403d3d6b4d2a556dc5dc11a85fc
ziard <61586725+grap1@users.noreply.github.com>
2021-09-19 20:21:51 +0530

    Add files via upload

---
+++ PhoenixMiner_5.7b_Windows/start_miner.bat
@@ -0,0 +1,14 @@
+REM
+REM Example bat file for starting PhoenixMiner.exe to mine ETH
+REM
+
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+
+REM IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+PhoenixMiner.exe -pool ssl://eu1.ethermine.org:5555 -pool2 ssl://us1.ethermine.org:5555 -wal 0xcBCEAAD63d5F9962FCCFD4bdbF69b559AD898B96.Rig001
+pause
+
